d0353214915366cf46b6cd3376bbd4255fac1428
clackgot <clackgot@gmail.com>
2021-06-19 04:23:08 +0300

    Добавлена библиотека методов

--- RIAA.4/Form1.cs
+++ RIAA.4/Form1.cs
@@ -1,135 +1,76 @@
 ﻿using System;
 using System.Windows.Forms;
 namespace RIAA._4
-{    public partial class Form1 : Form    {
+{
+    public partial class Form1 : Form
+    {
         public Form1()
         {
             InitializeComponent();
         }
-        //<summary>
-        //  Метод Симпсона
-        //</summary>
+        private static string functionName = "(1 + Cos(x))";
+
+
         private void tabPage1_button1_Click(object sender, EventArgs e)
         {
-            double I,  I1,  a = ((double)tabPage1_numericUpDown1.Value), 
-                b = ((double)tabPage1_numericUpDown2.Value), 
-                eps = ((double)tabPage1_numericUpDown3.Value);
-            I = eps + 1; I1 = 0;
-            for (int N = 2; (N <= 4) || (Math.Abs(I1 - I) > eps); N *= 2)
-            {
-                double h, sum2 = 0, sum4 = 0, sum = 0;
-                h = (b - a) / (2 * N);
-                for (int i = 1; i <= 2 * N - 1; i += 2)
-                {
-                    sum4 += f(a + h * i);
-                    sum2 += f(a + h * (i + 1));
-                }
-                sum = f(a) + 4 * sum4 + 2 * sum2 - f(b);  I = I1;  I1 = (h / 3) * sum; }
+            double a = ((double)tabPage1_numericUpDown1.Value);//Левая граница интегрирования
+            double b = ((double)tabPage1_numericUpDown2.Value);//Правая граница интегрирования
+            double eps = ((double)tabPage1_numericUpDown3.Value);//Точность эпсилон
             tabPage1_richTextBox1.Text = "";
-            tabPage1_richTextBox1.Text += $"    dx\n---------------  = {I1}\n(1+Cos(x))";
+            tabPage1_richTextBox1.Text += $"    dx\n---------------  = {Lib.SimpsonMethod(a, b, eps)}\n{functionName}";
         }
+
+
+        /// <summary>
+        /// Основная функция
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         double f(double x)
         {
             return x * Math.Pow(Math.E, x);
         }
-        double Integ(double a, double b, int n, double y)
-        {
-            return ((b - a) / (2 * n) * y);
-        }
-        double integral_l(double a, double b, int n)
-        {
-            double x, h=(b - a) / n; double sum = 0.0;
-            double fx;
-            for (int i = 0; i < n; i++)
-            { x = a + i * h;   fx = f(x);  sum += fx;    }
-            return (sum * h);
-        }
-        double integral_r(double a, double b, int n)
-        {
-            double x, h;  double sum = 0.0;  double fx;
-            h = (b - a) / n;
-            for (int i = 1; i <= n; i++)
-            {   x = a + i * h;  fx = f(x);    sum += fx; }
-            return (sum * h);
-        }
-        //<summary>
-        // Метод трапеций
-        //</summary>
+        
+
         private void tabPage2_button1_Click(object sender, EventArgs e)
         {
             int n = ((int)tabPage2_numericUpDown3.Value);
-            double In,
-                a = ((double)tabPage2_numericUpDown1.Value),
-                b = ((double)tabPage2_numericUpDown2.Value),
-                dy = (b - a) / n,  y = 0;         y += f(a) + f(b);
-            for (int i = 1; i < n; i++)
-            {                y += 2 * (f(a + dy * i));
-            }            In = Integ(a, b, n, y);
+            double a = ((double)tabPage2_numericUpDown1.Value), b = ((double)tabPage2_numericUpDown2.Value);
             tabPage2_richTextBox1.Text = "";
-            tabPage2_richTextBox1.Text += $"    dx\n---------------  = {In}\n(1+Cos(x))";
+            tabPage2_richTextBox1.Text += $"    dx\n---------------  = {Lib.TrapezoidMethod(a, b, n)}\n{functionName}";
         }
-        //<summary>
-        //  Метод левых прямоугольников.
-        //</summary>
+
+
+
+
         private void tabPage3_button2_Click(object sender, EventArgs e)
-        {            double a = ((double)tabPage3_numericUpDown4.Value),
-                b = ((double)tabPage3_numericUpDown5.Value),
-                eps = ((double)tabPage3_numericUpDown6.Value);
-            double s1, s;
-            int n = 1;
-            s1 = integral_l(a, b, n);
-            do
-            {
-                s = s1;
-                n = 2 * n;
-                s1 = integral_l(a, b, n); 
-            } while (Math.Abs(s1 - s) > eps);
+        {
+            double a = ((double)tabPage3_numericUpDown4.Value), b = ((double)tabPage3_numericUpDown5.Value), eps = ((double)tabPage3_numericUpDown6.Value);
             tabPage3_richTextBox2.Text = "";
-            tabPage3_richTextBox2.Text += $"    dx\n---------------  = {s1}\n(1+Cos(x))";
+            tabPage3_richTextBox2.Text += $"    dx\n---------------  = {Lib.LeftRectangleMethod(a, b, eps)}\n{functionName}";
         }
-        //<summary>
-        //  Метод правых прямоугольников.
-        //</summary>
+
+
+
         private void tabPage4_button3_Click(object sender, EventArgs e)
         {
-            double a = ((double)tabPage4_numericUpDown7.Value),
-                b = ((double)tabPage4_numericUpDown8.Value),
-                eps = ((double)tabPage4_numericUpDown9.Value);
-            double s1, s;            int n = 1;
-            s1 = integral_r(a, b, n);
-            do            {                s = s1;
-                n = 2 * n;
-                s1 = integral_r(a, b, n);
-            } while (Math.Abs(s1 - s) > eps);
+            double a = ((double)tabPage4_numericUpDown7.Value), b = ((double)tabPage4_numericUpDown8.Value), eps = ((double)tabPage4_numericUpDown9.Value);
+
+            
             tabPage4_richTextBox3.Text = "";
-            tabPage4_richTextBox3.Text += $"    dx\n---------------  = {s1}\n(1+Cos(x))";
+            tabPage4_richTextBox3.Text += $"    dx\n---------------  = {Lib.RightRectangleMethod(a, b, eps)}\n{functionName}";
         }
-        //<summary>
-        //  Метод средних прямоугольников.
-        //</summary>
+
+
         private void tabPage5_button4_Click(object sender, EventArgs e)
         {
-            double a = ((double)tabPage5_numericUpDown10.Value),
-                b = ((double)tabPage5_numericUpDown11.Value),
-                eps = ((double)tabPage5_numericUpDown12.Value),
-                n = ((double)tabPage5_numericUpDown1.Value),
-                s,                 s1,
-            h = b - a;
-            s = h * f(((a + b) / 2));
-            do
-            {
-                h = (b - a) / n;
-                s1 = s;
-                s = 0;
-                for (int k = 0; k <= n - 1; k++)
-                {
-                    s = s + f((a + h / 2 + k * h));
-                }
-                s = s * h;
-            } while (((Math.Abs(s - s1)) / 3) > eps);
+            double a = ((double)tabPage5_numericUpDown10.Value), b = ((double)tabPage5_numericUpDown11.Value), eps = ((double)tabPage5_numericUpDown12.Value), n = ((double)tabPage5_numericUpDown1.Value);
+
+            
             tabPage5_richTextBox4.Text = "";
-            tabPage5_richTextBox4.Text += $"    dx\n---------------  = {s}\n(1+Cos(x))";
-            var date1 = new DateTime(2008, 5, 1, 8, 30, 52);
+            tabPage5_richTextBox4.Text += $"    dx\n---------------  = {Lib.MiddleRectangleMethod(a, b, eps, n)}\n{functionName}";
         }
+
+
     }
 }
---
+++ RIAA.4/Lib.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RIAA._4
+{
+    public static class Lib
+    {
+        private static double Integ(double a, double b, int n, double y)
+        {
+            return ((b - a) / (2 * n) * y);
+        }
+        private static double IntegrateLeft(double a, double b, int n)
+        {
+            double x, h = (b - a) / n; double sum = 0.0;
+            double fx;
+            for (int i = 0; i < n; i++)
+            { x = a + i * h; fx = f(x); sum += fx; }
+            return (sum * h);
+        }
+        private static double IntegrateRight(double a, double b, int n)
+        {
+            double x, h; double sum = 0.0; double fx;
+            h = (b - a) / n;
+            for (int i = 1; i <= n; i++)
+            { x = a + i * h; fx = f(x); sum += fx; }
+            return (sum * h);
+        }
+        /// <summary>
+        /// Основная функция
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private static double f(double x)
+        {
+            return x * Math.Pow(Math.E, x);
+        }
+        //<summary>
+        // Метод средних прямоугольников.
+        //</summary>
+        public static double MiddleRectangleMethod(double a, double b, double eps, double n)
+        {
+            double s, s1, h = b - a;
+            s = h * f(((a + b) / 2));
+            do
+            {
+                h = (b - a) / n;
+                s1 = s;
+                s = 0;
+                for (int k = 0; k <= n - 1; k++)
+                {
+                    s = s + f((a + h / 2 + k * h));
+                }
+                s = s * h;
+            } while (((Math.Abs(s - s1)) / 3) > eps);
+            return s;
+        }
+        /// <summary>
+        /// Метод правых прямоугольников
+        /// </summary>
+        public static double RightRectangleMethod(double a, double b, double eps)
+        {
+            //double a = ((double)tabPage4_numericUpDown7.Value), b = ((double)tabPage4_numericUpDown8.Value), eps = ((double)tabPage4_numericUpDown9.Value);
+            double s1, s; int n = 1;
+            s1 = IntegrateRight(a, b, n);
+            do
+            {
+                s = s1;
+                n = 2 * n;
+                s1 = IntegrateRight(a, b, n);
+            } while (Math.Abs(s1 - s) > eps);
+            return s1;
+        }
+        /// <summary>
+        /// Метод левых прямоугольников
+        /// </summary>
+        public static double LeftRectangleMethod(double a, double b, double eps)
+        {
+
+            double s1, s;
+            int n = 1;
+            s1 = IntegrateLeft(a, b, n);
+            do
+            {
+                s = s1;
+                n = 2 * n;
+                s1 = IntegrateLeft(a, b, n);
+            } while (Math.Abs(s1 - s) > eps);
+            return s1;
+        }
+        /// <summary>
+        /// Метод трапеций
+        /// </summary>
+        public static double TrapezoidMethod(double a, double b, int n)
+        {
+
+            double In, dy = (b - a) / n, y = 0; y += f(a) + f(b);
+            for (int i = 1; i < n; i++)
+            {
+                y += 2 * (f(a + dy * i));
+            }
+            In = Integ(a, b, n, y);
+
+            return In;
+        }
+        /// <summary>
+        /// Метод Симпсона
+        /// </summary>
+        public static double SimpsonMethod(double a, double b, double eps)
+        {
+            double I, I1;
+
+            I = eps + 1; I1 = 0;
+            for (int N = 2; (N <= 4) || (Math.Abs(I1 - I) > eps); N *= 2)
+            {
+                double h, sum2 = 0, sum4 = 0, sum = 0;
+                h = (b - a) / (2 * N);
+                for (int i = 1; i <= 2 * N - 1; i += 2)
+                {
+                    sum4 += f(a + h * i);
+                    sum2 += f(a + h * (i + 1));
+                }
+                sum = f(a) + 4 * sum4 + 2 * sum2 - f(b); I = I1; I1 = (h / 3) * sum;
+            }
+            return I1;
+        }
+    }
+}
